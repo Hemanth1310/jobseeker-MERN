248fb680001240a7af9578be180f6b641ab31a75
Hemanth1310 <hemanth13k@gmail.com>
2026-05-05 16:57:48 +0200

    backend-index-listener

---
+++ backend/src/index.ts
@@ -0,0 +1,11 @@
+
+import express from "express"
+import AuthRouter from './authRoutes.js'
+
+const app = express()
+app.use(express.json())
+app.use('/auth',AuthRouter)
+
+app.listen(3003,()=>{
+    console.log("listening at 3003")
+})
