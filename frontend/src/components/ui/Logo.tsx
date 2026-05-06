1949f999b2a37b0a480cc099953e8b102e5e81d2
Hemanth1310 <hemanth13k@gmail.com>
2026-05-06 14:12:24 +0200

    frontend-logo-component

---
+++ frontend/src/components/ui/Logo.tsx
@@ -0,0 +1,12 @@
+import { Snail } from 'lucide-react'
+import React from 'react'
+
+const Logo = () => {
+  return (
+     <div className='h-full w-8 bg-brand-primary rounded-lg flex items-center justify-center'>
+        <Snail  color='rgb(224 231 255)'/>
+    </div>
+  )
+}
+
+export default Logo
