01a1c7640f293a4a853c4c541d6a58cd4be8b8bd
Hemanth1310 <hemanth13k@gmail.com>
2026-05-07 13:36:08 +0200

    frontend-zod-schemas

---
+++ frontend/src/utils/TypeChecker.ts
@@ -0,0 +1,25 @@
+import z from 'zod'
+
+export const ROLE_VALUES = ['CANDIDATE', 'EMPLOYER'] as const
+export const roleSchema = z.enum(ROLE_VALUES)
+
+const emailRules = z.string().email("Invalid email format").trim().toLowerCase()
+
+// Define password rules ONCE so they are identical everywhere
+const passwordRules = z.string()
+    .min(8, "Please enter at least 8 characters")
+    .regex(/[!@#$%^&*]/, "Please enter atleast one special charecter e.g. !@#$%^&*");
+
+
+export const registerSchema = z.object({
+    email: emailRules,
+    password: passwordRules,
+    firstName: z.string().min(1,'Firstname cannot be empty'),
+    lastName: z.string().min(1,'Lastname cannot be empty'),
+    role: roleSchema
+})
+
+export const loginSchema = z.object({
+    email: emailRules,
+    password: passwordRules
+})
