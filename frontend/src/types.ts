6a16732006112c5cdc2cf948fb96f536a4044eb5
Hemanth1310 <hemanth13k@gmail.com>
2026-05-07 11:35:55 +0200

    frontend-user-types-update

---
+++ frontend/src/types.ts
@@ -0,0 +1,11 @@
+
+export type Role = "CANDIDATE" | "EMPLOYER"
+
+export type User= {
+    email: string;
+    id: string;
+    firstName: string;
+    lastName: string;
+    role: Role;
+    createdAt: Date;
+}
