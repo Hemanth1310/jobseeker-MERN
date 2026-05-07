89cae761591b4b6c933188c015f6f2fd9950d9af
Hemanth1310 <hemanth13k@gmail.com>
2026-05-07 12:08:06 +0200

    frontend-home-update

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,10 +9,10 @@ function App() {
 
 
   return (
-    <div className='w-screen min-h-screen bg-indigo-50 p-5'>
+    <div className='w-screen min-h-screen flex flex-col bg-indigo-50 p-5'>
       <HeaderLayout/>
-      <div className="p-5 flex-1 w-full flex gap-5 bg-mist-50 border border-mist-200 rounded-2xl">
-          <div className="flex-5 min-h-full rounded-2xl p-5 box-border">
+      <div className="p-5 flex-1 w-full h-full flex gap-5 bg-mist-50 border border-mist-200 rounded-2xl">
+          <div className="min-h-full w-full rounded-2xl p-5 box-border">
             <Routes>
               <Route path="/" element={<Home/>}></Route>
              
