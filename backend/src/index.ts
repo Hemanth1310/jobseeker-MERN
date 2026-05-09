84d3b2df3540c67ad7e67589d7dc23cd988abe0b
Hemanth1310 <hemanth13k@gmail.com>
2026-05-09 16:56:11 +0200

    backend-cors-update

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,11 +1,26 @@
 
 import express from "express"
 import AuthRouter from './authRoutes.js'
-
+import cors from 'cors'
 const app = express()
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
+
 app.use(express.json())
 app.use('/api/auth',AuthRouter)
 
 app.listen(3003,()=>{
     console.log("listening at 3003")
 })
+
