3888d4ac70687c46799bafb079d148fcd636a49d
Hemanth1310 <hemanth13k@gmail.com>
2026-05-19 15:01:08 +0200

    frontend-jobcard

--- frontend/src/components/layouts/JobCard.tsx
+++ frontend/src/components/layouts/JobCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import type { jobDataType } from '../../utils/TypeChecker'
+import { Edit } from 'lucide-react'
 
 type Props = {
     jobPosting: jobDataType,
@@ -8,10 +9,15 @@ type Props = {
 
 const JobCard = ({jobPosting, children}: Props) => {
   return (
-    <div className='flex flex-col  border-2 border-mist-200 p-10 rounded-lg '>
-        <div>
-            <h1>{jobPosting.title}</h1>
-            <p>{jobPosting.companyName}</p>
+    <div className='flex flex-col justify-between border-2 border-mist-200 p-5 rounded-lg '>
+        <div className='flex justify-between'>
+            <div className='flex flex-col gap-2'>
+                <h1 className='text-xl font-semibold'>{jobPosting.title}</h1>
+                <p className='text-mist-500 text-sm'>{jobPosting.companyName}</p>
+            </div>
+            <div>
+                <Edit size={28}/>
+            </div>
         </div>
         {children}
         
