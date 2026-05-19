7f68b2a0d8f38f1ddaa8fb3a51986647b992d463
Hemanth1310 <hemanth13k@gmail.com>
2026-05-19 15:00:39 +0200

    frontend-decoupling-jobform-type

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -22,3 +22,18 @@ export type JobPosts = {
     isActive: boolean,
     id:string
 }
+
+export interface JobFormState {
+  title: string;
+  description: string;
+  companyName: string;
+  location: string;
+  jobType: string;      
+  experience: string;   
+  category: string;     
+  salary: string;       
+}
+
+export interface JobFormErrors extends JobFormState{
+    apiResponse:string;
+}
