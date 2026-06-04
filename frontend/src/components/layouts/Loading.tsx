d8cb448dcd5ba1d8236288c98263f2ce19b1b54a
Hemanth1310 <hemanth13k@gmail.com>
2026-06-04 12:56:51 +0200

    frontend-loading-time-fix

--- frontend/src/components/layouts/Loading.tsx
+++ frontend/src/components/layouts/Loading.tsx
@@ -1,8 +1,12 @@
+import { Loader } from 'lucide-react'
 import React from 'react'
 
 const Loading = () => {
   return (
-    <div className='w-full h-full flex items-center justify-center'>Loading</div>
+    <div className='w-full h-full flex items-center justify-center'>
+      <Loader className="animate-spin [animation-duration:4s] text-brand-primary" />
+      Loading
+    </div>
   )
 }
 
