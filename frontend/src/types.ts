ca67d62c8f21a24cb3a2e57ff50a28a824328c72
Hemanth1310 <hemanth13k@gmail.com>
2026-06-05 15:10:31 +0200

    frontend-types-applications

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -1,6 +1,8 @@
 export type toggeler = "login"|"register"
 export type Role = "CANDIDATE" | "EMPLOYER"
-
+export type JobType= "Fulltime" | "Internship" | "Freelance"
+export type Experience="Experienced" | "Intermediate" | "Begginer"
+export type Category= "Software" | "Design" | "Sales" | "Marketing" | "Finance"
 
 
 export interface JobFormState {
@@ -16,4 +18,35 @@ export interface JobFormState {
 
 export interface JobFormErrors extends JobFormState{
     apiResponse:string;
+}
+
+export type Status = "PENDING" | "REVIEWING" | "ACCEPTED" | " REJECTED"
+
+export type candidateApplications = {
+    job: {
+        id: string;
+        createdAt: Date;
+        title: string;
+        description: string;
+        companyName: string;
+        location: string;
+        jobType: JobType;
+        experience: Experience;
+        category: Category;
+        isActive: boolean;
+        salary: number | null;
+        employerId: string;
+    };
+} & {
+    id: string;
+    jobId: string;
+    candidateId: string;
+    coverLetter: string;
+    EarliestStartDate: Date;
+    countryOfResidence: string;
+    ValidWorkPermit: boolean;
+    resumePath: string;
+    status: Status;
+    feedback: string;
+    appliedAt: Date;
 }
