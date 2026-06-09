96fa33df209b1049d259f662720d7225f48b31ae
Hemanth1310 <hemanth13k@gmail.com>
2026-06-09 14:40:09 +0200

    frontend-css-fix-header-responsive

--- frontend/src/components/layouts/HeaderLayout.tsx
+++ frontend/src/components/layouts/HeaderLayout.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useRef, useState } from 'react'
 import Logo from '../ui/Logo'
 import { useAuthContextData } from '../../utils/useAuthContextData'
 import AuthLayout from './AuthLayout'
-import { ChevronDown, ChevronUp, CircleUserRound, LogOut } from 'lucide-react'
+import { ChevronDown, ChevronUp, CircleUserRound, LogOut, Menu } from 'lucide-react'
 import axios from '../../utils/authMiddleware'
 import { NavLink } from 'react-router'
 
@@ -13,6 +13,7 @@ const HeaderLayout = () => {
   const [isOpen, setIsOpen] = useState(false)
   const [isDropDownOpen, setIsDropDownOpen] = useState(false)
   const dropDownRef = useRef<HTMLDivElement>(null)
+  const [isMenuOpen, setIsMenuOpen] = useState(false)
   const handleLogout=async()=>{
       try{
         await axios.get(`${BASE_API_URL}/api/private/logout`)
@@ -34,6 +35,9 @@ const HeaderLayout = () => {
   },[])
   return (
     <div className='w-full  h-16 flex items-center justify-between pr-5 pl-5 font-mono'>
+        <div className='flex sm:hidden' onClick={()=>setIsMenuOpen(prev=>!prev)}>
+            <Menu />
+        </div>
         <div className='h-8 flex gap-2 items-center'>
              <Logo/>
             <div className='text-xl'>
@@ -43,23 +47,57 @@ const HeaderLayout = () => {
         <div>
           {userData?
             <div className='flex gap-5 items-center'>
-                {userData.role==='EMPLOYER'?
-                <div>
-                  <NavLink to={'/employer/dashboard'} className={({isActive})=>`p-1 pl-5 pr-5 font-light text-sm hover:text-indigo-600 ${isActive && 'border-2 border-indigo-400 rounded-lg bg-indigo-200'}`}>
-                    Openings
-                  </NavLink>
-                  <NavLink to='/employer/make-a-post' className={({isActive})=>`p-1 pl-5 pr-5 font-light text-sm hover:text-indigo-600  ${isActive && 'border-2 border-indigo-400 rounded-lg bg-indigo-100'}`}>
-                    Make a post
-                  </NavLink>
+                <div className='hidden sm:flex'>
+                    {userData.role==='EMPLOYER'?
+                        <div >
+                          <NavLink to={'/employer/dashboard'} className={({isActive})=>`p-1 pl-5 pr-5 font-light text-sm hover:text-indigo-600 ${isActive && 'border-2 border-indigo-400 rounded-lg bg-indigo-200'}`}>
+                            Openings
+                          </NavLink>
+                          <NavLink to='/employer/make-a-post' className={({isActive})=>`p-1 pl-5 pr-5 font-light text-sm hover:text-indigo-600  ${isActive && 'border-2 border-indigo-400 rounded-lg bg-indigo-100'}`}>
+                            Make a post
+                          </NavLink>
 
-                </div>:<div>
-                    <NavLink to={'/dashboard'} className={({isActive})=>`p-1 pl-5 pr-5 font-light text-sm hover:text-indigo-600 ${isActive && 'border-2 border-indigo-400 rounded-lg bg-indigo-200'}`}>
-                      Dashboard
-                    </NavLink>
-                    <NavLink to='/MyApplications' className={({isActive})=>`p-1 pl-5 pr-5 font-light text-sm hover:text-indigo-600  ${isActive && 'border-2 border-indigo-400 rounded-lg bg-indigo-100'}`}>
-                      My Applications
-                    </NavLink>
-                  </div>}
+                        </div>:<div>
+                            <NavLink to={'/dashboard'} className={({isActive})=>`p-1 pl-5 pr-5 font-light text-sm hover:text-indigo-600 ${isActive && 'border-2 border-indigo-400 rounded-lg bg-indigo-200'}`}>
+                              Dashboard
+                            </NavLink>
+                            <NavLink to='/MyApplications' className={({isActive})=>`p-1 pl-5 pr-5 font-light text-sm hover:text-indigo-600  ${isActive && 'border-2 border-indigo-400 rounded-lg bg-indigo-100'}`}>
+                              My Applications
+                            </NavLink>
+                          </div>}
+                  </div>
+                   {isMenuOpen && 
+                   
+                   <div className='absolute top-0 left-0 w-screen h-screen z-100 bg-mist-50 shadow-md border border-mist-200 p-5 rounded-lg flex flex-col'>
+                          <div className='flex justify-between'>
+                              <div className='h-8 flex gap-2 items-center'>
+                                  <Logo/>
+                                  <div className='text-xl'>
+                                    Job/seeker
+                                  </div>
+                              </div>
+                              <div onClick={()=>setIsMenuOpen(false)}>X</div>
+                          </div>
+                          <div className='mt-5'>
+                                {userData.role==='EMPLOYER'?
+                                  <div className='flex flex-col' onClick={()=>setIsMenuOpen(false)}>
+                                    <NavLink to={'/employer/dashboard'} className={({isActive})=>`p-2 pl-5 pr-5 font-bold text-lg hover:text-indigo-600 ${isActive && 'border-2 border-indigo-400 rounded-lg bg-indigo-200'}`}>
+                                      Openings
+                                    </NavLink>
+                                    <NavLink to='/employer/make-a-post' className={({isActive})=>`p-2 pl-5 pr-5 font-bold text-lg hover:text-indigo-600  ${isActive && 'border-2 border-indigo-400 rounded-lg bg-indigo-100'}`}>
+                                      Make a post
+                                    </NavLink>
+
+                                  </div>:<div className='flex flex-col' onClick={()=>setIsMenuOpen(false)}>
+                                      <NavLink to={'/dashboard'} className={({isActive})=>`p-2 pl-5 pr-5 font-bold text-sm hover:text-indigo-600 ${isActive && 'border-2 border-indigo-400 rounded-lg bg-indigo-200'}`}>
+                                        Dashboard
+                                      </NavLink>
+                                      <NavLink to='/MyApplications' className={({isActive})=>`p-2 pl-5 pr-5 font-bold text-sm hover:text-indigo-600  ${isActive && 'border-2 border-indigo-400 rounded-lg bg-indigo-100'}`}>
+                                        My Applications
+                                      </NavLink>
+                                    </div>}
+                          </div>
+                    </div>}
                   <div ref={dropDownRef} className='relative flex items-center' onClick={(e)=>{e.stopPropagation();setIsDropDownOpen(prev=>!prev)}}>
                     <CircleUserRound size={28}/>
                     {isDropDownOpen?<ChevronUp size={18}/>:<ChevronDown size={18}/>}
